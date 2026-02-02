b7a2766e6cf966c6b434e65174a5410b84e0df0b
swethadomatoti <swethadomatoti@gmil.com>
2026-02-02 14:48:16 +0530

    Add dj-database-url for Postgres

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -5,6 +5,7 @@ Django settings for blogsite project.
 from pathlib import Path
 import os
 import logging
+import dj_database_url
 
 try:
     from dotenv import load_dotenv
@@ -67,13 +68,14 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'blogsite.wsgi.application'
+ 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
